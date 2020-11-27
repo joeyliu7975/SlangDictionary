69200dc82f7bed140ac98fe85ac1e9d6e0661f99
Joey <joeyliu7975@gmail.com>
2020-11-27 14:11:31 +0800

    Create xcode project

---
+++ MandarinUrbanDictionary/MandarinUrbanDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MandarinUrbanDictionary
+//
+//  Created by Joey Liu on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

8859aa1ace54fc341622bcd89df0d53dddececc7
Joey <joeyliu7975@gmail.com>
2020-12-01 22:49:59 +0800

    compose AddNewWordViewController categoryTF with a pickerView

--- MandarinUrbanDictionary/MandarinUrbanDictionary/Controller/New Word/AddNewWordViewController.swift
+++ MandarinUrbanDictionary/MandarinUrbanDictionary/Controller/New Word/AddNewWordViewController.swift
@@ -15,6 +15,23 @@ class AddNewWordViewController: UIViewController {
     @IBOutlet weak var categoryTF: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     
+    let categoryList = Category.allCases
+    
+    private var pickerView: UIPickerView? {
+        didSet {
+            guard let picker = pickerView else { return }
+            
+            picker.delegate = self
+            picker.dataSource = self
+            
+            categoryTF.inputView = picker
+            
+            let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+            
+            view.addGestureRecognizer(tap)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,8 +39,30 @@ class AddNewWordViewController: UIViewController {
         setup()
     }
     
+    @objc func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
     @IBAction func clickSend(_ sender: UIButton) {
         
+    }
+}
+
+extension AddNewWordViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        return categoryTF.text = categoryList[row].title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryList[row].title
     }
 }
 
@@ -34,5 +73,7 @@ private extension AddNewWordViewController {
         
         view.backgroundColor = .separatorlineBlue
         containerView.backgroundColor = .separatorlineBlue
+        
+        pickerView = UIPickerView()
     }
 }

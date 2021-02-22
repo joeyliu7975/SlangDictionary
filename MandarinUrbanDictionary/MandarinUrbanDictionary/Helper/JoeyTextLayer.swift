92e19d8fea353c458eeda3ef09381647ef58873b
JiaXin Liu <joeyliu7975@gmail.com>
2021-02-22 22:29:59 +0800

    remove comment from JoeyTextLayer

--- MandarinUrbanDictionary/MandarinUrbanDictionary/Helper/JoeyTextLayer.swift
+++ MandarinUrbanDictionary/MandarinUrbanDictionary/Helper/JoeyTextLayer.swift
@@ -9,17 +9,13 @@ import UIKit
 
 class JoeyTextLayer: CATextLayer {
 
-    // REF: http://lists.apple.com/archives/quartz-dev/2008/Aug/msg00016.html
-    // CREDIT: David Hoerl - https://github.com/dhoerl
-    // USAGE: To fix the vertical alignment issue that currently exists within the CATextLayer class. Change made to the yDiff calculation.
-
     override func draw(in context: CGContext) {
         let height = self.bounds.size.height
         let fontSize = self.fontSize
         let yDiff = (height-fontSize)/2 - fontSize/10
 
         context.saveGState()
-        context.translateBy(x: 0, y: yDiff) // Use -yDiff when in non-flipped coordinates (like macOS's default)
+        context.translateBy(x: 0, y: yDiff)
         super.draw(in: context)
         context.restoreGState()
     }
